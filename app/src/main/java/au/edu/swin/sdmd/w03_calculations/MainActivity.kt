8c5bd669dfb0aa2620f8612cf4e685454d765ac5
Elvis Le <elvishaile120@gmail.com>
2024-04-15 00:02:58 +1000

    Completed app as of Part 1

--- app/src/main/java/au/edu/swin/sdmd/w03_calculations/MainActivity.kt
+++ app/src/main/java/au/edu/swin/sdmd/w03_calculations/MainActivity.kt
@@ -2,11 +2,14 @@ package au.edu.swin.sdmd.w03_calculations
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.RadioButton
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private var operator = "plus"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,13 +19,36 @@ class MainActivity : AppCompatActivity() {
         val answer = findViewById<TextView>(R.id.answer)
 
         val equals = findViewById<Button>(R.id.equals)
+
         equals.setOnClickListener {
-            val result = add(number1.text.toString().toInt(), number2.text.toString().toInt())
-            // TODO: show result on the screen
+            val result = when(operator) {
+                "plus" -> add(number1.text.toString(), number2.text.toString())
+                "sub" -> sub(number1.text.toString(), number2.text.toString())
+                 else -> add(number1.text.toString(), number2.text.toString())
+            }
             answer.text = result.toString()
+        }
+    }
+    fun onRadioButtonClicked(view: View) {
+        if (view is RadioButton) {
+            val checked = view.isChecked
+
+
+            when (view.getId()) {
+                R.id.radioPlus ->
+                    if (checked) {
+                        operator = "plus"
+                    }
+
+                R.id.radioMinus ->
+                    if (checked) {
+                        operator = "sub"
+                    }
+            }
         }
     }
 
     // adds two numbers together
-    private fun add(number1: Int, number2: Int) = number1 + number2
+    private fun add(number1: String, number2: String) = number1.toInt() + number2.toInt()
+    private fun sub(number1: String, number2: String) = number1.toInt() - number2.toInt()
 }
