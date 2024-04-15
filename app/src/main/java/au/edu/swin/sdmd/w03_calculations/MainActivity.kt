b489972d6ad7806f281a03419ccc01d00d9f59c2
Elvis Le <elvishaile120@gmail.com>
2024-04-15 12:39:17 +1000

    Lifecycle implementation

--- app/src/main/java/au/edu/swin/sdmd/w03_calculations/MainActivity.kt
+++ app/src/main/java/au/edu/swin/sdmd/w03_calculations/MainActivity.kt
@@ -10,49 +10,8 @@ import android.widget.RadioButton
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    private var operator = "plus"
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        Log.d("Lifecycle","onCreate")
-        setContentView(R.layout.activity_main)
-
-        val number1 = findViewById<EditText>(R.id.number1)
-        val number2 = findViewById<EditText>(R.id.number2)
-        val answer = findViewById<TextView>(R.id.answer)
-
-        val equals = findViewById<Button>(R.id.equals)
-
-        equals.setOnClickListener {
-            val result = when(operator) {
-                "plus" -> add(number1.text.toString(), number2.text.toString())
-                "sub" -> sub(number1.text.toString(), number2.text.toString())
-                 else -> add(number1.text.toString(), number2.text.toString())
-            }
-            answer.text = result.toString()
-        }
-    }
-    fun onRadioButtonClicked(view: View) {
-        if (view is RadioButton) {
-            val checked = view.isChecked
-
-
-            when (view.getId()) {
-                R.id.radioPlus ->
-                    if (checked) {
-                        operator = "plus"
-                    }
-
-                R.id.radioMinus ->
-                    if (checked) {
-                        operator = "sub"
-                    }
-            }
-        }
-    }
-
-    // adds two numbers together
-    private fun add(number1: String, number2: String) = number1.toInt() + number2.toInt()
-    private fun sub(number1: String, number2: String) = number1.toInt() - number2.toInt()
+    var calculation: Int = 0
+    var operator = "plus"
 
     override fun onStart() {
         super.onStart()
@@ -83,4 +42,59 @@ class MainActivity : AppCompatActivity() {
         super.onDestroy()
         Log.d("Lifecycle","onDestroy")
     }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d("Lifecycle","onCreate")
+        setContentView(R.layout.activity_main)
+
+        val number1 = findViewById<EditText>(R.id.number1)
+        val number2 = findViewById<EditText>(R.id.number2)
+        val answer = findViewById<TextView>(R.id.answer)
+
+        savedInstanceState?.let {
+            calculation = it.getInt("answer")
+            answer.text = calculation.toString()
+        }
+
+        val equals = findViewById<Button>(R.id.equals)
+
+        equals.setOnClickListener {
+            calculation = when(operator) {
+                "plus" -> add(number1.text.toString(), number2.text.toString())
+                "sub" -> sub(number1.text.toString(), number2.text.toString())
+                 else -> add(number1.text.toString(), number2.text.toString())
+            }
+            answer.text = calculation.toString()
+        }
+    }
+    fun onRadioButtonClicked(view: View) {
+        if (view is RadioButton) {
+            val checked = view.isChecked
+
+
+            when (view.getId()) {
+                R.id.radioPlus ->
+                    if (checked) {
+                        operator = "plus"
+                    }
+
+                R.id.radioMinus ->
+                    if (checked) {
+                        operator = "sub"
+                    }
+            }
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt("answer", calculation)
+        Log.i("Lifecycle","saveInstanceState $calculation")
+    }
+
+    // adds two numbers together
+    private fun add(number1: String, number2: String) = number1.toInt() + number2.toInt()
+    private fun sub(number1: String, number2: String) = number1.toInt() - number2.toInt()
+
+
 }
